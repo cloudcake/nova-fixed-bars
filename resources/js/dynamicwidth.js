3ac05e9355a5cf3c53acdbdbca6c4b206de98314
Stephen Lake <stephen@closurecode.com>
2019-04-28 01:25:06 +0200

    Minor format clean up

--- resources/js/dynamicwidth.js
+++ resources/js/dynamicwidth.js
@@ -1,23 +1,28 @@
 function calibrateWidths() {
     const sidebar = document.querySelector('.w-sidebar')
     const contentHeader = document.querySelector('.content .h-header')
-    const content = document.querySelector('.content')
+    const content = document.querySelector('#nova .content')
 
     sidebar.style = 'height: 100%'
+
     contentHeader.style.marginLeft = 0
     contentHeader.style.width = '100%'
+
     content.style.marginLeft = `${sidebar.offsetWidth}px`
     content.style.width = '100%'
     content.style.maxWidth = '100%'
 
     document
         .querySelector('meta[name=viewport]')
-        .setAttribute('content', 'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0')
+        .setAttribute(
+            'content',
+            'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0'
+        )
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     calibrateWidths()
-}, false);
+});
 
 window.onresize = function(event) {
     calibrateWidths()

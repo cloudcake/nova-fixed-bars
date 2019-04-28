add41b51fbf4321af17f49d77904e0a206621203
Stephen Lake <stephen@closurecode.com>
2019-04-28 03:32:05 +0200

    Fix header positioning

--- resources/js/dynamicwidth.js
+++ resources/js/dynamicwidth.js
@@ -1,29 +1,33 @@
+window.addEventListener('DOMContentLoaded', function() {
+    calibrateWidths()
+})
+
+window.addEventListener('load', function() {
+    calibrateWidths()
+})
+
+window.addEventListener('resize', function() {
+    calibrateWidths()
+})
+
 function calibrateWidths() {
-    const sidebar = document.querySelector('.w-sidebar')
-    const contentHeader = document.querySelector('.content .h-header')
-    const content = document.querySelector('#nova .content')
-
-    sidebar.style = 'height: 100%'
-
-    contentHeader.style.marginLeft = 0
-    contentHeader.style.width = '100%'
-
-    content.style.marginLeft = `${sidebar.offsetWidth}px`
-    content.style.width = '100%'
-    content.style.maxWidth = '100%'
-
     document
         .querySelector('meta[name=viewport]')
         .setAttribute(
             'content',
             'width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0'
         )
+
+    const sidebar = document.querySelector('.w-sidebar')
+    const contentHeader = document.querySelector('.content .h-header')
+    const content = document.querySelector('#nova .content')
+
+    sidebar.style = 'height: 100%'
+
+    content.style.marginLeft = sidebar.offsetWidth+'px'
+    content.style.width = '100%'
+    content.style.maxWidth = '100%'
+
+    contentHeader.style.left = sidebar.offsetWidth+'px'
+    contentHeader.style.width = '100%'
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    calibrateWidths()
-});
-
-window.onresize = function(event) {
-    calibrateWidths()
-};
